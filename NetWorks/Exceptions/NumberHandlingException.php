23ccf922d701e8be3f90720c380b447dc812f3e3
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2025-02-06 11:35:58 -0500

    Delete NetWorks directory

--- NetWorks/Exceptions/NumberHandlingException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace networks\Exception;
-use Exception;
-class NumberHandlingException extends Exception{
-    public function __construct(string $message, int $code=0) {
-        parent::__construct($message,$code);
-    }
-}
-?>
--- NetWorks/themes/default/js/online.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(()=>{
-    setInterval(()=>{
-        (new Request(`${toRelPath()}assets/php/user.php?action=online`)).send();
-    },1000);
-});
